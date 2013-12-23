cc0dfaef4d6493a048b4915f81c8eb6a237feb14
troop <troop@email.de>
2013-12-23 11:37:07 +0100

    added touch event and reworked drawing part

--- src/com/troop/freecam/controls/StyleAbelSlider.java
+++ src/com/troop/freecam/controls/StyleAbelSlider.java
@@ -5,9 +5,12 @@ import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.troop.freecam.HDR.BitmapHandler;
@@ -21,7 +24,7 @@ public class StyleAbelSlider extends View
     //the activity
     Context context;
     //the sliderimage stored in memory
-    Bitmap sliderImage;
+    Drawable sliderImage;
     //the minmal value of the slider
     int min;
     //the max value of the slider
@@ -30,7 +33,7 @@ public class StyleAbelSlider extends View
     int current;
     int sliderWidth;
     int sliderHeight;
-    int drawPosition;
+    Rect drawPosition;
     boolean horizontal;
 
     int picID;
@@ -73,34 +76,121 @@ public class StyleAbelSlider extends View
 
         if (sliderImage == null)
         {
-            if (horizontal)
-            {
-                sliderImage = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(context.getResources(), picID), this.getHeight(), this.getHeight(), false);
-            }
-            else
-            {
-                sliderImage = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(context.getResources(), picID), this.getWidth(), this.getWidth(), false);
-            }
+            sliderImage = getResources().getDrawable(picID);// Bitmap.createScaledBitmap(BitmapFactory.decodeResource(context.getResources(), picID), this.getHeight(), this.getHeight(), false);
+            getPosToDraw();
         }
         if (sliderImage != null)
-            canvas.drawBitmap(sliderImage, getPosToDraw().left, getPosToDraw().top, new Paint());
+        {
+            sliderImage.setBounds(drawPosition.left, drawPosition.top, drawPosition.right, drawPosition.bottom);
+            sliderImage.draw(canvas);
+        }
+            //canvas.drawBitmap(sliderImage, getPosToDraw().left, getPosToDraw().top, new Paint());
     }
 
     private Rect getPosToDraw()
     {
+        Rect ret;
         if (horizontal)
         {
             int pos = getWidth() / max * current;
-            int half = sliderWidth / 2;
-            Rect ret = new Rect(pos - half, 0, sliderWidth, sliderHeight);
-            return ret;
+            int half = getHeight() / 2;
+            ret = new Rect(pos - half, 0, getHeight() + pos - half, getHeight());
         }
         else
         {
             int pos = getHeight() /max *current;
-            int half = sliderHeight / 2;
-            Rect ret = new Rect(0, pos - sliderHeight , sliderWidth, sliderHeight);
-            return ret;
+            int half = getWidth() / 2;
+            ret = new Rect(0, pos - half , getWidth(), getWidth() + pos - half);
         }
+        drawPosition = ret;
+        return ret;
+    }
+
+    private int getValueFromDrawingPos(int posi)
+    {
+        int val;
+        if (horizontal)
+        {
+            val = posi / max * current;
+        }
+        else
+        {
+            val = posi / max * current;
+        }
+        current = val;
+        return val;
+    }
+
+    public void SetCurrentValue(int pos)
+    {
+        if (pos <= max && pos >= min)
+        {
+            current = pos;
+            invalidate();
+        }
+    }
+
+    public void SetMaxValue(int max)
+    {
+        this.max = max;
+        if (current > max)
+            current = max;
+        invalidate();
+    }
+
+    private void setNewDrawingPos(int val)
+    {
+        if (horizontal)
+        {
+            int half = getHeight() / 2;
+            Rect tmp = new Rect(val - half, 0 , getHeight() + val - half, getHeight());
+            if (tmp.left >= 0 && tmp.right <= getWidth())
+                drawPosition = tmp;
+        }
+        else
+        {
+            int half = getWidth() / 2;
+            Rect tmp = new Rect(0, val - half , getWidth(), getWidth() + val - half);
+            if (tmp.top >= 0 && tmp.bottom <= getHeight())
+                drawPosition = tmp;
+        }
+    }
+
+    boolean sliderMoving = false;
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        boolean throwevent = false;
+        switch (event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                if (drawPosition.contains((int)event.getX(), (int)event.getY()))
+                {
+                    sliderMoving = true;
+                }
+                throwevent = true;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (sliderMoving)
+                {
+                    if (horizontal)
+                    {
+                        setNewDrawingPos((int) event.getX());
+                    }
+                    else
+                    {
+                        setNewDrawingPos((int) event.getY());
+                    }
+                    invalidate();
+                    throwevent = true;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                if (sliderMoving)
+                    sliderMoving = false;
+                throwevent = false;
+                break;
+        }
+        return throwevent;
     }
 }
